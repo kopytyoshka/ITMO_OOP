6c24d0a3684bfa6135e8dec015b962f8d59be024
Alexey Kopytin <ilyatttt19@gmail.com>
2021-12-24 20:16:05 +0300

    commit fixed Exceptions

--- Banks/Accounts/Deposit.cs
+++ Banks/Accounts/Deposit.cs
@@ -1,5 +1,6 @@
 using System;
 using Banks.Entities;
+using Banks.Tools;
 
 namespace Banks.Accounts
 {
@@ -10,12 +11,12 @@ namespace Banks.Accounts
 
         public override void BetweenBankAccounts(Customer customer, BankAccount bankAccountFrom, BankAccount bankAccountTo, double money)
         {
-            throw new Exception("Operation declined due to it's a Deposit Account");
+            throw new BankException("Operation declined due to it's a Deposit Account");
         }
 
         public override void CashWithdrawal(Customer customer, BankAccount bankAccount, double money)
         {
-            throw new Exception("Operation declined due to it's a Deposit Account");
+            throw new BankException("Operation declined due to it's a Deposit Account");
         }
     }
 }
